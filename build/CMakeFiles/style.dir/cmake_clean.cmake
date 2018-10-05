f2c12f2d790a3d6ac2438f423ed15cf684fcf454
Sohal Patel <sohal@kodezine.com>
2018-10-05 11:10:42 +0200

    Build folder removed and added in ignore list

--- build/CMakeFiles/style.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/style"
-  "vera_noreport_0.xml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/style.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/style_reports.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/style_reports"
-  "vera_report_0.xml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/style_reports.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
